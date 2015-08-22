3c0e739be401f1737854a6f51a4b04b314e70bfa
Andrew Breckenridge <asbreckenridge@me.com>
2015-08-22 15:47:24 -0700

    create proj

---
+++ Dopamine/Dopamine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dopamine
+//
+//  Created by Andrew Breckenridge on 8/22/15.
+//  Copyright © 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
